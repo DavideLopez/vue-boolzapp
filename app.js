e1ba727e82d0fe10bf20f261ac7c850bbc1c4274
DavideLopez <davidelopez7@gmail.com>
2022-08-27 17:22:42 +0200

    aggiunto primo contatto

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 console.log('boolzap')
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        contacts: [{
+            id: 1,
+            nome: 'giorgia',
+            avatar: '/avatar1',
+            visible: true,
+            messages: [
+               {
+                 date: '27/08/2022 12:33:46',
+                 message: 'hai infornato la lasagna?',
+                 status: 'sent',
+               },
+               {
+                date: '27/08/2022 12:33:46',
+                message: 'ricordati la sveglia!',
+                status: 'sent',
+              },
+              {
+                date: '27/08/2022 12:33:46',
+                message: 'certo!',
+                status: 'sent',
+              },
+
+            ]
+             
+        }]
+    }
+})
+
+            
